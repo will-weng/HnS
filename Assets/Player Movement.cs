b2238e5021a843b9f859d7c8089e5bd3b90f3b3c
mainstorageb <mainstorageb@gmail.com>
2024-01-08 22:33:21 +1100

    registering horizontal and vertical inputs

--- Assets/Player Movement.cs
+++ Assets/Player Movement.cs
@@ -6,16 +6,28 @@ public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed; 
     public Rigidbody2D rb;
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
     {
-        
+        ProcessInputs();
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    void ProcessInputs()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+
+        moveDirection = new Vector2(moveX, moveY);
+    }
+
+    void Move()
+    {
+        rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed).normalized;
     }
 }
