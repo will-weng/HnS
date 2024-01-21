88832745ae15a7a986e5165a14e5c8b53670e34d
will-weng <william.weng@hotmail.com>
2024-01-21 15:41:21 +1100

    imported Text mesh

---
+++ Assets/Scripts/Steamworks.NET/SteamLobby.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SteamLobby : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject multiplayerUI
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
