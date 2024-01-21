289e1709070c00a2eefdf0eb1f20f0b73dc5be33
will-weng <william.weng@hotmail.com>
2024-01-21 15:27:47 +1100

    Cleaned up Network manager and build setting

--- Assets/Scripts/MyNetworkManager.cs
+++ Assets/Scripts/MyNetworkManager.cs
@@ -1,6 +1,6 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using Mirror;
 
 public class CustomNetworkManager : NetworkManager
